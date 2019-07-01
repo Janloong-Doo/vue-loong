0c6a231a5d57423d3844b2a9866d642c0a1e02cc
Janloong-Doo <807110586@qq.com>
2019-07-01 18:08:54 +0800

    update

--- src/views/Study.vue
+++ src/views/Study.vue
@@ -23,7 +23,11 @@
       <todolist v-for="(a,i) in todo " v-bind:key="i" v-bind:todo-name="a"></todolist>
     </div>
     <div>
-      <table border="1">
+      <el-table border="1">
+        <el-row>
+          <el-table-column label="1"/>
+          <el-table-column label="2"/>
+        </el-row>
         <tr>
           <td>1</td>
           <td>2</td>
@@ -32,12 +36,12 @@
           <td>a</td>
           <td>b</td>
         </tr>
-      </table>
+      </el-table>
     </div>
     <div>
       <input v-model.trim="requesturl" placeholder="请求url"><br>
-      <button v-on:click="requestUrl">提交
-      </button>
+      <el-button v-on:click="requestUrl">提交
+      </el-button>
       <br>
       <a>{{resultD}}</a>
     </div>
@@ -46,6 +50,8 @@
 <script>
   import todolist from "../components/TodoList";
   import axios from 'axios';
+  import qs from 'qs';
+  import {Message} from 'element-ui';
 
   export default {
     name: "study",
@@ -77,51 +83,32 @@
       },
       requestUrl() {
         if (this.requesturl === '' || this.requesturl == null) {
-          // this.requesturl = "http://localhost:8080/login"
-          this.requesturl = "http://168.130.1.33:11001/authcenter/sysUser/login"
+          // this.requesturl = "http://168.130.1.33:11001/authcenter/sysUser/login"
+          this.requesturl = "https://localhost:8088/head"
         }
-        let param = {
-          'username': "admin",
-          'password': "admin"
+        let params = {
+          name: 'admin',
+          age: '25'
         };
         let config = {
-          header: {
-            'Content-Type': 'application/x-www-form-urlencoded'
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            // 'Content-Type': 'application/json'
+            // 'Authorization':'a'    //非'简单请求'时会自动发送一个探测请求
           }
         };
 
-        axios.post(
-          this.requesturl,
-          {
-            username: "admin",
-            password: "admin"
-          },
-          {
-            header: {
-              'Content-Type': 'application/x-www-form-urlencoded'
-            }
-          }
-        ).then(res => {
+        axios.post(this.requesturl, qs.stringify(params), config).then(res => {
           console.log("成功");
           console.log(res.data);
+          // Message.success("成功");
           this.resultD = res.data;
         }).catch(res => {
           console.log("失败");
           console.log(res.data);
           this.resultD = res.data;
-
-        })
-        // then(function (response) {
-        //   console.log("成功");
-        //   console.log(response)
-        //   // if (success) {
-        //   //   console.log(response)
-        //   // }
-        // })
-        //   .catch(function (response) {
-        //   console.log("错误");
-        //   console.log(response)
-        // })
+          // Message.error("失败")
+        });
       }
     }
     ,
